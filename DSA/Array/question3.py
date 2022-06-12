9616e0c459bd780fb55b949ba977faa91b69fe19
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-06-12 23:27:22 +0530

    swap alternative elements in a array.

---
+++ DSA/Array/question3.py
@@ -0,0 +1,15 @@
+# swap alternative elements in an array.
+def alter_swap(arr):
+    i = 0
+    if len(arr) %2 ==0:
+        while i < len(arr):
+            arr[i],arr[i+1] = arr[i+1],arr[i]
+            i += 2
+    else:
+         while i < len(arr)-1:
+            arr[i],arr[i+1] = arr[i+1],arr[i]
+            i += 2
+    return arr
+arr = [1,2,3,4,5,6]
+alter_swap(arr)
+print(arr)
