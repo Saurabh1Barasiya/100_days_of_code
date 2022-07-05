6240ed28b04843a5b8f48484c969a5a379fd9006
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-07-05 17:03:29 +0530

    Rotate array by k elements

---
+++ DSA/Array/question18.py
@@ -0,0 +1,21 @@
+# Rotate array by k elements.
+def rotate(arr,k):
+    temp = [0 for i in arr]
+    i = 0
+    while i < len(arr):
+        temp[(i+k)%len(arr)] = arr[i]
+        i += 1
+    #arr = temp
+    #return arr 
+
+    # copy temp to arr
+    for index,value in enumerate(temp):
+        arr[index] = value
+
+    return arr
+
+if __name__ == '__main__':
+    arr = [1,7,9,11]
+    k = 2
+    ans = rotate(arr,k)
+    print(ans)
