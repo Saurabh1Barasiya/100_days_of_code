a4fc88fd75dfc50e15e0f0f7a721c0d5bd869736
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-07-09 11:46:50 +0530

    Sting methods

---
+++ DSA/String/question1.py
@@ -0,0 +1,15 @@
+# capatalize 
+a = "amit"
+print("capitalize--> first letter ko capital kar degi ---> ",a.capitalize())
+
+a = "AMAN"
+print("Case fold--> uppercase ko lower me convert kar degi. --> ",a.casefold())
+
+c = "           amit                "
+print(c,len(c))
+
+txt = "banana"
+print(txt.center(50,'*'))
+
+print("-------------------------------------")
+print(a)
