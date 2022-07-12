e1996c88f2bd6081010cb63f4d78725c8d607f3c
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-07-12 08:19:57 +0530

    def get_sum(matrix,row,col):

---
+++ DSA/2-D-Array/question9.py
@@ -0,0 +1,19 @@
+# get sum of 2-dimensional array.
+def get_sum(matrix,row,col):
+    sum_ = 0
+    for i in range(row):
+        for j in range(col):
+            sum_ += matrix[i][j]
+    return sum_
+
+
+if __name__ == '__main__':
+    matrix = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+    row = 3
+    col = 3
+    ans = get_sum(matrix, row, col)
+    print(ans)
