842e2576cfb15f21548fdf22b5c1506ee6a21710
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-07-16 17:02:09 +0530

    # get gcd --> greatest common divisor // Heights common divisor HCF.

--- sorting/shraddha.py
+++ sorting/shraddha.py
@@ -1,3 +1,11 @@
-s = input()
-print(len(set(s)))
+# get gcd --> greatest common divisor // Heights common divisor HCF.
 
+
+def gcd(a,b):
+    maxi = a if a>b else b
+    for i in range(maxi,1,-1):
+        if a%i==0 and b%i==0:
+            return i
+
+ans = gcd(10,15)
+print(ans)
